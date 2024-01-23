7904e2a3cba0a08219558d476dfa644f32ea420d
andrew <andrew@cloud-latitude.com>
2024-01-23 13:36:10 +0800

    add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
@@ -7,4 +7,7 @@ export default defineConfig({
     port: 3000,
   },
   plugins: [react()],
+  test:{
+    environment: 'jsdom',
+  }
 })
