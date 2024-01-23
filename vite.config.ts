f2b7db6dbd197550f5e52a41ec9189f3f3a67d5f
andrew <andrew@cloud-latitude.com>
2024-01-23 13:58:09 +0800

    add test

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,16 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    port: 3000,
-  },
-  plugins: [react()],
-  test:{
-    environment: 'jsdom',
-  }
+    server: {
+        port: 3000,
+    },
+    plugins: [react()],
+    test: {
+        environment: 'jsdom',
+        globals: true,
+        setupFiles: './tests/setup.ts', // assuming the test folder is in the root of our project
+        include: ['**/*.test.tsx', '**/*.test.ts', '**/*.test.js', '**/*.test.jsx'],
+        exclude: ['**/node_modules/**', '**/dist/**', '**/cypress/**', '**/.{idea,git,cache,output,temp}/**'],
+    }
+
 })
